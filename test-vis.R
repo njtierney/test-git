725052b6604ea1e7311491d9c978c95f50b0007c
tierneyn <nicholas.tierney@gmail.com>
2015-07-17 11:25:51 +1000

    This is my first commit using RSTudio. Let's hope it goes well!!

---
+++ test-vis.R
@@ -0,0 +1,7 @@
+# This is a test for Git practice with R.  Maybe this will be useful? Who knows.
+
+library(ggplot2)
+library(plyr)
+library(dplyr)
+
+qplot(Sepal.Width, Sepal.Length, data = iris, colour = Species)
