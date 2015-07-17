ae5fd7241c596d914552e6352aaf56b8b17e7eef
tierneyn <nicholas.tierney@gmail.com>
2015-07-17 11:43:06 +1000

    Now added a plot.

--- test-vis.R
+++ test-vis.R
@@ -6,4 +6,12 @@ library(dplyr)
 
 qplot(Sepal.Width, Sepal.Length, data = iris, colour = Species)
 
-# So hopefully this all works.
+# So hopefully this all works.# So hopefully this all works.
+
+# some quick linear modelling
+
+iris.lm <- lm(Sepal.Width ~ ., data  =iris)
+
+summary(iris.lm)
+
+plot(iris.lm)
